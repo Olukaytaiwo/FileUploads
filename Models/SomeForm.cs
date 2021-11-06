c2ce74cf98dbe063dcf0a5551f5da03f960ec085
olukaytaiwo <olukaytaiwo@yahoo.com>
2021-11-06 06:53:22 +0100

    Add project files.

---
+++ Models/SomeForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace FileUploads.Models
+{
+    public class SomeForm
+    {
+        public string Name { get; set; }
+        public IFormFile File { get; set; }
+
+    }
+}
